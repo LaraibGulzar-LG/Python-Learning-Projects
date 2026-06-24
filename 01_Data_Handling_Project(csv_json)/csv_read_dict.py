b0d8915ddb80c8b3178d4f18ed65076915720ec9
Laraib Gulzar <55898073+LaraibGulzar-LG@users.noreply.github.com>
2026-06-25 02:43:45 +0500

    Delete 01_Data_Handling_Project(csv_json) directory

--- 01_Data_Handling_Project(csv_json)/csv_read_dict.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open('students.csv', 'r') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        print(row['name'], row['age'], row['city'])
--- 01_Data_Handling_Project(csv_json)/json_write.py
+++
@@ -1,6 +0,0 @@
-import json
-
-new_data = {"status": "success", "count": 5}
-
-with open('output.json', 'w') as file:
-    json.dump(new_data, file, indent=2)
